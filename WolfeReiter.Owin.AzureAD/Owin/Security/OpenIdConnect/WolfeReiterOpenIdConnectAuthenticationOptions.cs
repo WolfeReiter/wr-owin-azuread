0a3a715f452581b89ecaf14291c52076ced7525f
Brian Reiter <breiter@wolfereiter.com>
2018-10-11 12:14:12 +0200

    Back out "RequireNonce=false" mitigation with comments.

--- WolfeReiter.Owin.AzureAD/Owin/Security/OpenIdConnect/WolfeReiterOpenIdConnectAuthenticationOptions.cs
+++ WolfeReiter.Owin.AzureAD/Owin/Security/OpenIdConnect/WolfeReiterOpenIdConnectAuthenticationOptions.cs
@@ -14,14 +14,21 @@ namespace WolfeReiter.Owin.Security.OpenIdConnect
         {
             ProtocolValidator = new OpenIdConnectProtocolValidator()
             {
-                NonceLifetime = TimeSpan.FromMinutes(15),
+                NonceLifetime = TimeSpan.FromMinutes(15) //,
                 /*****************************************
                  * Rare event, but blocks authentication *
                  *****************************************/
                 //Event Message: [Microsoft.IdentityModel.Protocols.OpenIdConnectProtocolInvalidNonceException] 
                 //IDX10311: RequireNonce is 'true' (default) but validationContext.Nonce is null. A nonce cannot be validated. 
                 //If you don't need to check the nonce, set OpenIdConnectProtocolValidator.RequireNonce to 'false'.
-                RequireNonce = false
+                //
+                // Possible Scenarios:
+                // 
+                // 1. This browser is configured to block cookies.
+                // 2. Application is embedded in an iframe in another website and the user has disabled 3rd party cookies.
+                // 3. The browser has an adblocker extension installed that is preventing the nonce cookie from being returned.
+
+                //RequireNonce = false
             };
         }
     }
